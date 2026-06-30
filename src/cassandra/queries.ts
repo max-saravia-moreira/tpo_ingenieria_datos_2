fca34fcc7fc243a0ad71c4653161ae92395c874e
Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2026-06-30 18:21:13 -0300

    feat: remove unnecessary attributes

--- src/cassandra/queries.ts
+++ src/cassandra/queries.ts
@@ -2,8 +2,8 @@ import { cassandra } from "./index.js";
 
 export async function listDriversByGeohash(geohash: string) {
   return cassandra.execute(
-    `SELECT nombre, licencia, calificacion, viajes_total, conductor_activo,
-            patente, marca, modelo, color, tipo_vehiculo, vehiculo_activo
+    `SELECT nombre, licencia, calificacion, viajes_total,
+            patente, marca, modelo, color, tipo_vehiculo
      FROM conductores_por_zona
      WHERE geohash = ?
      LIMIT 5`,
